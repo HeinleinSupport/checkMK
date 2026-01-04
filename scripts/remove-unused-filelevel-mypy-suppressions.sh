8dc21c7d35a8ba02485bbc95618440345de3d6f6
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-01-05 00:02:34 +0100

    add script to remove unused suppressions
    Change-Id: I67770337e7814f992f2e3bb9cda07e120646fbbc

---
+++ scripts/remove-unused-filelevel-mypy-suppressions.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+# Small helper to remove all unused file level mypy suppressions.
+# For every suppressions type:
+#  * remove all suppressions
+#  * run mypy
+#  * reset all files for which this causes errros
+#  * create a commit for the changed files
+#
+# Run from repo root.
+#
+
+TMPFILE="$(mktemp)"
+
+run_mypy() {
+    echo "Running: bazel build --config=mypy ..."
+    bazel build --config=mypy ...
+}
+
+list_files_with_suppression() {
+    ag "^# mypy: disable-error-code=\"$1\"" -l
+}
+
+list_all_used_suppression_types() {
+    ag --nofilename -o '(?<=^# mypy: disable-error-code=")[^"]*' | sort | uniq
+}
+
+extract_failed_files() {
+    # cp "${1}" "${1}.${2}.failed"  # for debugging
+    grep ": error:" "${1}" | cut -d: -f1 | uniq
+}
+
+remove_suppression() {
+    st="$1"
+    # remove all suppressions
+    echo "Removing '# mypy: disable-error-code=\"${st}\"'"
+    # shellcheck disable=SC2046  # we want word splitting here
+    sed -i "/# mypy: disable-error-code=\"${st}\"/d" $(list_files_with_suppression "${st}")
+    echo "Changed files: $(git df | wc -l)"
+
+    # Run linters and restore failures
+    while ! run_mypy &>"${TMPFILE}"; do
+        echo "Checking failing files back out"
+        # shellcheck disable=SC2046  # we want word splitting here
+        git checkout $(extract_failed_files "${TMPFILE}" "${st}")
+        echo "Changed files: $(git df | wc -l)"
+    done
+    # shellcheck disable=SC2046  # we want word splitting here
+    bazel run //:format $(git diff --name-only --pretty=)
+    git commit -am "Remove unnecessary mypy suppression: ${st}"
+}
+
+main() {
+    echo "Removing unused mypy suppressions. This will take a while."
+    for st in $(list_all_used_suppression_types); do
+        echo "====================== ${st} =========================="
+        remove_suppression "${st}"
+    done
+    rm -f "${TMPFILE}"
+}
+
+main
