50367a9d6cefc86e5c9120b2cc2b49424a83c280
Salvatore Nitopi <salvatore.nitopi@checkmk.com>
2026-01-22 15:37:11 +0100

    CMK-28984 - Fix issues in cmk-cert
    * update cert-rotation composition test to check site certificate fingerprint on central site-ca rotation
    * add a warning saying that all agents need to be re-registered when a site-ca is rotated
    * remove the option to rotate 'agent-ca' certificates
    * set default certificate expiration to 10 years for site-ca and site certificates
    * update unit-tests
    * add feature to rotate remote site certificates
    * add composition test test_rotate_remote_site_certificate
    
    Change-Id: I28ef34069861d071e3dde4dff32920bdd90b5979

--- cmk/gui/cmkcert.py
+++ cmk/gui/cmkcert.py
@@ -17,7 +17,7 @@ from pathlib import Path
 from typing import Literal
 
 import cmk.utils.paths
-from cmk.ccc.site import SiteId
+from cmk.ccc.site import omd_site, SiteId
 from cmk.utils.certs import (
     agent_root_ca_path,
     cert_dir,
@@ -79,15 +79,14 @@ CA. After the finalization step, changes need to be activated in the GUI again t
 Site CA and site certificates.
 In a distributed monitoring setup, the rotation should be run from the central site to ensure \
 that remote sites' trust stores are updated automatically.
-
-'agent-ca' certificate: Rotating the Agent CA certificate is an experimental feature \
-and requires manual re-registration of all agents to trust the new certificate. Use with caution.
+Warning: following the Site CA certificate rotation, all agents will need to be manually \
+re-registered to trust the updated certificate.
 """,
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
     mode_rotate.add_argument(
         "target_certificate",
-        choices=["site", "site-ca", "agent-ca"],
+        choices=["site", "site-ca"],
         help="Specify which certificate to rotate.",
     )
 
@@ -97,8 +96,7 @@ and requires manual re-registration of all agents to trust the new certificate. 
         default=None,
         help=(
             "Specify the expiry time in days. "
-            "The default expiry time depends on the target certificate: 10 years for 'site-ca' "
-            "and 'agent-ca', 2 years for 'site' certificates."
+            "The default expiry time is 10 years for both 'site-ca' and 'site' certificates."
         ),
     )
 
@@ -106,10 +104,7 @@ and requires manual re-registration of all agents to trust the new certificate. 
         "--remote-site",
         dest="remote_site",
         type=str,
-        help=(
-            "'site-ca' certificate only -- "
-            "Specify the remote site id for which you want to rotate the certificate."
-        ),
+        help=("Specify the remote site id for which you want to rotate the certificate."),
     )
     mode_rotate.add_argument(
         "--finalize",
@@ -166,7 +161,10 @@ def _run_rotate(
     # we import the expensive GUI module only if we really need it
     import cmk.gui.cmkcert_rotate as rotate
 
-    if not (old_cert := _certificate_path(omd_root, site_id, target_certificate)).exists():
+    if (
+        site_id == omd_site()
+        and not (old_cert := _certificate_path(omd_root, site_id, target_certificate)).exists()
+    ):
         raise ValueError(f"Certificate '{target_certificate}' not found at '{old_cert}'.")
 
     match target_certificate:
@@ -185,15 +183,6 @@ def _run_rotate(
                     expiry=expiry,
                     key_size=4096,
                 )
-
-        case "agent-ca":
-            rotate.rotate_agent_ca_certificate(
-                omd_root=omd_root,
-                site_id=site_id,
-                expiry=expiry,
-                key_size=4096,
-            )
-
         case "site":
             rotate.rotate_site_certificate(
                 omd_root=omd_root,
@@ -201,6 +190,9 @@ def _run_rotate(
                 expiry=expiry,
                 key_size=4096,
             )
+        case "agent-ca":
+            sys.stdout.write("cmk-cert: Rotating the 'agent-ca' certificate is not supported.\n")
+            sys.exit(1)
 
 
 def main(args: Sequence[str]) -> int:
@@ -226,9 +218,9 @@ def main(args: Sequence[str]) -> int:
                         "cmk-cert: --expiry may only be used in the first step of rotating the 'site-ca' certificate, not when finalizing.\n"
                     )
                     return -1
-            elif parsed_args.remote_site or parsed_args.finalize:
+            elif parsed_args.finalize:
                 sys.stderr.write(
-                    "cmk-cert: --remote-site and --finalize can only be used when rotating the 'site-ca' certificate.\n"
+                    "cmk-cert: --finalize can only be used when rotating the 'site-ca' certificate.\n"
                 )
                 return -1
 
