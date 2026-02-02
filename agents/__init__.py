fdb1f833ea1498f7ffb29769429f9e47aae6c904
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-02-02 08:58:36 +0100

    remove obsolte init files
    These are leftovers of a workaround we
    no longer need.
    
    Change-Id: I5466216d673c3d4172a160d8b0617c8c84e5bbd3

--- agents/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
-# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
-# conditions defined in the file COPYING, which is part of this source code package.
-
-# mypy: disable-error-code="no-redef"
-
-# This file is only here to make pytest and mypy find the agent plugins.
-# It will not be shipped nor synced into the site. It must not contain code.
--- agents/plugins/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
-# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
-# conditions defined in the file COPYING, which is part of this source code package.
-
-# This file is only here to make pytest and mypy find the agent plugins.
-# It will not be shipped nor synced into the site. It must not contain code.
