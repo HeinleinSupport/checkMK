06569fdb6b9d2d4b4525ae132704ea635320efa0
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-02-02 15:24:32 +0100

    update validation sript towards LBD
    Change-Id: I742b7fefc04dd067a545f603d0ea79a8d927a288

--- doc/treasures/migration_helpers/legacy_checks/validation.sh
+++ doc/treasures/migration_helpers/legacy_checks/validation.sh
@@ -6,21 +6,21 @@
 # a) forgetting to run some of the validation steps
 # b) keep coming up with new commands that need approval
 
-cd "$(git rev-parse --show-toplevel)" || exit 1
+cd "$(git rev-parse --show-toplevel)" || exit $?
 
 # shellcheck disable=SC2046  # we want word splitting here
-bazel run //:format -- $(git diff --name-only) || exit 1
+bazel run //:format $(git diff --name-only) || exit $?
 
-bazel lint --fix //cmk || exit 1
+bazel lint --fix //cmk/... || exit $?
 
 # run mypy on more than the changed files. Takes longer, but coming back later takes even more time.
-scripts/run-uvenv mypy {tests/unit/,}cmk/base/{legacy_checks,check_legacy_includes} || exit 1
+bazel build --config=mypy //cmk/... || exit $?
 
 # ignore some that fail on collection.
 scripts/run-uvenv pytest tests/unit/cmk/base/legacy_checks tests/unit/cmk/plugins/ \
     --ignore tests/unit/cmk/plugins/metric_backend/ \
+    --ignore tests/unit/cmk/plugins/metric_backend_omd/ \
     --ignore tests/unit/cmk/plugins/custom_query_metric_backend/ \
-    --ignore tests/unit/cmk/plugins/clickhouse_omd/ \
-    --ignore tests/unit/cmk/plugins/ceph || exit 1
+    --ignore tests/unit/cmk/plugins/ceph || exit $?
 
 make -C tests test-plugins-consistency
