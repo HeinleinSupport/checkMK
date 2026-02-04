01e5ff7825f171aa870c7d20e19968e3f942652c
Mathias Laurin <mathias.laurin@checkmk.com>
2026-02-04 10:53:04 +0100

    Add test to check that python files are declared
    CMK-29546
    
    Change-Id: I0d4db2d830a11d21cacbec7ce3a3b7f802be27aa

---
+++ scripts/in_bazel.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+if [[ $# -ne 2 && $# -ne 3 ]]; then
+    echo "Usage: $0 <EXTENSION> <KIND> [<LIMIT>]" >&2
+    exit 1
+fi
+
+ext=$1
+kind=$2
+limit=${3:-0}
+
+out=$(
+    comm -23 \
+        <(
+            find . -type f -name "*.$ext" \
+                -not -path './bazel-*/*' \
+                -not -path './.git/*' \
+                -not -path './.venv/*' \
+                -not -path './doc/*' |
+                sed 's|^\./||' |
+                sort
+        ) \
+        <(
+            bazel cquery '
+            kind("source file", deps(kind("'"$kind"'", //...)))
+        ' |
+                grep -E "\.$ext " |
+                sed -E 's| \([^)]*\)$||; s|^//([^:]+):|\1/|' |
+                sort
+        )
+)
+
+count=$(echo "$out" | wc -l)
+echo "Found $count \".$ext\" files not declared as $kind"
+echo "-----"
+echo "$out"
+
+if ((count > limit)); then
+    exit 1
+fi
