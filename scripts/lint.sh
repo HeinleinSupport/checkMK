2d61f5c4ba14f48e4c7823ed475e22e2ee282843
Mathias Laurin <mathias.laurin@checkmk.com>
2026-02-09 16:00:01 +0100

    Instrument lint-repository job for debugging
    Change-Id: I3b2c70653f4dcbfa4ce42b19c1659a4e3a9c504a

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,5 +4,6 @@ set +e
 bazel lint --machine --fixes=false //... -- >lint.txt 2>&1
 exit_code=$?
 set -e
+cat lint.txt
 bazel run //scripts:sarif_preparse -- --root="$(bazel info workspace)"/bazel-bin --output "$(bazel info workspace)/results.sarif" || true
 exit $exit_code
