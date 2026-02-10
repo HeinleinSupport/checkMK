bd1c31414c125c154540e5dd589f2238ed83207e
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-02-10 11:17:32 +0100

    Legacy check migration: apc_symmetra_ext_temp I
    Change-Id: I3191599370bd40c9d2df005781a72d6672749f8d

--- cmk/base/legacy_checks/apc_symmetra_ext_temp.py
+++ cmk/base/legacy_checks/apc_symmetra_ext_temp.py
@@ -3,48 +3,62 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from collections.abc import Iterable
-from typing import Any
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition, LegacyResult
-from cmk.agent_based.v2 import SNMPTree, StringTable
-from cmk.base.check_legacy_includes.temperature import check_temperature, TempParamType
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.apc.lib_ats import DETECT
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
-check_info = {}
 
-
-def discover_apc_symmetra_ext_temp(info: StringTable) -> Iterable[tuple[str, dict[str, Any]]]:
-    for index, status, _temp, _temp_unit in info:
+def discover_apc_symmetra_ext_temp(section: StringTable) -> DiscoveryResult:
+    for index, status, _temp, _temp_unit in section:
         if status == "2":
-            yield index, {}
+            yield Service(item=index)
 
 
 def check_apc_symmetra_ext_temp(
-    item: str, params: TempParamType, info: StringTable
-) -> LegacyResult:
-    for index, _status, temp, temp_unit in info:
+    item: str, params: TempParamType, section: StringTable
+) -> CheckResult:
+    for index, _status, temp, temp_unit in section:
         if item == index:
-            unit = "f" if temp_unit == "2" else "c"
-            return check_temperature(
-                int(temp), params, "apc_symmetra_ext_temp_%s" % item, dev_unit=unit
+            yield from check_temperature(
+                reading=int(temp),
+                params=params,
+                unique_name=item,
+                value_store=get_value_store(),
+                dev_unit="f" if temp_unit == "2" else "c",
             )
+            return
 
-    return 3, "Sensor not found in SNMP data"
+    yield Result(state=State.UNKNOWN, summary="Sensor not found in SNMP data")
 
 
 def parse_apc_symmetra_ext_temp(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["apc_symmetra_ext_temp"] = LegacyCheckDefinition(
+snmp_section_apc_symmetra_ext_temp = SimpleSNMPSection(
     name="apc_symmetra_ext_temp",
-    parse_function=parse_apc_symmetra_ext_temp,
     detect=DETECT,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.318.1.1.10.2.3.2.1",
         oids=["1", "3", "4", "5"],
     ),
+    parse_function=parse_apc_symmetra_ext_temp,
+)
+
+
+check_plugin_apc_symmetra_ext_temp = CheckPlugin(
+    name="apc_symmetra_ext_temp",
     service_name="Temperature External %s",
     discovery_function=discover_apc_symmetra_ext_temp,
     check_function=check_apc_symmetra_ext_temp,
