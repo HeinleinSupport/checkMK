be9146dec8a2ba4abe5a291fdcbafe05bf1c7e3e
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2026-02-10 09:26:53 +0100

    Adapt bazel-lint after 27d4e53e (checkmk_ci)
    Change-Id: Ib52c907fcdeb7a936b970b45065980b54b38317b

--- buildscripts/scripts/test-bazel-lint.groovy
+++ buildscripts/scripts/test-bazel-lint.groovy
@@ -4,12 +4,16 @@
 
 void main() {
     def test_jenkins_helper = load("${checkout_dir}/buildscripts/scripts/utils/test_helper.groovy");
+    def versioning = load("${checkout_dir}/buildscripts/scripts/utils/versioning.groovy");
+    def safe_branch_name = versioning.safe_branch_name();
+    def container_safe_branch_name = safe_branch_name.replace(".", "-");
 
     dir("${checkout_dir}") {
         test_jenkins_helper.execute_test([
             name: "test-bazel-lint",
             cmd: "bazel run //:buildifier.check | tee bazel-lint.txt 2>&1",
             output_file: "bazel-lint.txt",
+            container_name: "ubuntu-2404-${container_safe_branch_name}-latest",
         ]);
 
         test_jenkins_helper.analyse_issues("BAZELLINT", "bazel-lint.txt");
