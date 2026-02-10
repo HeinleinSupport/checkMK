ce2351f48ec44f97c668c539a87d73fac90c9d9b
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2026-02-10 09:39:57 +0100

    Lint chain needs flex in the container
    see https://tribe29.slack.com/archives/C085MHGLPJ6/p1770712689481909?thread_ts=1770539080.707519&cid=C085MHGLPJ6
    requires https://review.lan.tribe29.com/c/checkmk_ci/+/126188
    
    Change-Id: I55079533f66079afd5edea9ef199221796627861

--- buildscripts/scripts/lint-repository.groovy
+++ buildscripts/scripts/lint-repository.groovy
@@ -4,12 +4,16 @@
 
 void main() {
     def test_jenkins_helper = load("${checkout_dir}/buildscripts/scripts/utils/test_helper.groovy");
+    def versioning = load("${checkout_dir}/buildscripts/scripts/utils/versioning.groovy");
+    def safe_branch_name = versioning.safe_branch_name();
+    def container_safe_branch_name = safe_branch_name.replace(".", "-");
 
     dir("${checkout_dir}") {
         test_jenkins_helper.execute_test([
             name       : "lint-repository",
             cmd        : "scripts/lint.sh",
             output_file: "results.sarif",
+            container_name: "ubuntu-2404-${container_safe_branch_name}-latest",
         ]);
         test_jenkins_helper.analyse_issues("SARIF", "results.sarif");
     }
