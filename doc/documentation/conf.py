79d1e7431afb4cfd07845dafb881cb605857bbed
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2026-02-13 11:37:30 +0100

    Purge empty/unused directory
    Change-Id: I7d5de840f4de8002a1fcf18e2737f1c0d4991c27

--- doc/documentation/conf.py
+++ doc/documentation/conf.py
@@ -80,11 +80,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 #
 html_theme = "alabaster"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
 language = "en"
 
 # Replaces e.g. "Checkmk's" with characters that can not correctly be rendered
