403e17814b6cb9b45a6d4cdc8d7a86980f81303d
Sven Panne <sven.panne@checkmk.com>
2026-02-16 14:39:51 +0100

    Make ruff happy again
    Change-Id: Icb2fdce9e95f88fbeb9d6c6f9810fb34decf40a6

--- scripts/sarif_preparse.py
+++ scripts/sarif_preparse.py
@@ -25,7 +25,7 @@ def load_all(root_dir: Path) -> SarifFileSet:
             with file_path.open("rt") as fd:
                 sarif_fd = SarifFile(file_path, json.load(fd))
                 try:
-                    sarif_results = sarif_fd.runs[0].get_results()
+                    sarif_fd.runs[0].get_results()
                 except KeyError as exc:
                     sys.stderr.write(f"No results found:\n\t{file_path!s}: {exc!s}\n")
                     continue
