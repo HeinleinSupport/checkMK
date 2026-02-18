ab572fb99c649aa675dddcc36d15de51b26b3c35
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-02-18 23:34:31 +0100

    Legacy check migration: couchbase_buckets_fragmentation  I and _cache II
    Change-Id: I432d5732d73caa3ca22d4fab9bfa9025743824bf

--- cmk/legacy_checks/couchbase_buckets_fragmentation.py
+++ cmk/legacy_checks/couchbase_buckets_fragmentation.py
@@ -3,31 +3,39 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-# mypy: disable-error-code="type-arg"
 
+from collections.abc import Mapping
+from typing import Any
 
-from collections.abc import Iterable
-
-from cmk.agent_based.legacy.v0_unstable import check_levels, LegacyCheckDefinition
-from cmk.agent_based.v2 import render
+from cmk.agent_based.legacy.conversion import (
+    # Temporary compatibility layer untile we migrate the corresponding ruleset.
+    check_levels_legacy_compatible as check_levels,
+)
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    render,
+    Service,
+)
 from cmk.plugins.couchbase.lib import parse_couchbase_lines, Section
-
-check_info = {}
-
-DiscoveryResult = Iterable[tuple[str, dict]]
 
 
 def discover_couchbase_buckets_fragmentation(section: Section) -> DiscoveryResult:
-    yield from ((item, {}) for item, data in section.items() if "couch_docs_fragmentation" in data)
+    yield from (
+        Service(item=item) for item, data in section.items() if "couch_docs_fragmentation" in data
+    )
 
 
-def check_couchbase_buckets_fragmentation(item, params, parsed):
-    if not (data := parsed.get(item)):
+def check_couchbase_buckets_fragmentation(
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    if not (data := section.get(item)):
         return
     docs_fragmentation = data.get("couch_docs_fragmentation")
     if docs_fragmentation is not None:
-        yield check_levels(
+        yield from check_levels(
             docs_fragmentation,
             "docs_fragmentation",
             params.get("docs"),
@@ -37,7 +45,7 @@ def check_couchbase_buckets_fragmentation(item, params, parsed):
 
     views_fragmentation = data.get("couch_views_fragmentation")
     if views_fragmentation is not None:
-        yield check_levels(
+        yield from check_levels(
             views_fragmentation,
             "views_fragmentation",
             params.get("views"),
@@ -46,11 +54,17 @@ def check_couchbase_buckets_fragmentation(item, params, parsed):
         )
 
 
-check_info["couchbase_buckets_fragmentation"] = LegacyCheckDefinition(
+agent_section_couchbase_buckets_fragmentation = AgentSection(
     name="couchbase_buckets_fragmentation",
     parse_function=parse_couchbase_lines,
+)
+
+
+check_plugin_couchbase_buckets_fragmentation = CheckPlugin(
+    name="couchbase_buckets_fragmentation",
     service_name="Couchbase Bucket %s Fragmentation",
     discovery_function=discover_couchbase_buckets_fragmentation,
     check_function=check_couchbase_buckets_fragmentation,
     check_ruleset_name="couchbase_fragmentation",
+    check_default_parameters={},
 )
