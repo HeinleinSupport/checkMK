687f82e2a74ac487f2540dadea1fbbe05c47375b
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-02-19 08:25:33 +0100

    Legacy check migration: couchbase_buckets_items I
    Change-Id: If735417c6eab068858d32f5f4b513389c8943cd5

--- cmk/legacy_checks/couchbase_buckets_items.py
+++ cmk/legacy_checks/couchbase_buckets_items.py
@@ -3,30 +3,36 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-# mypy: disable-error-code="type-arg"
 
+from collections.abc import Mapping
+from typing import Any
 
-from collections.abc import Iterable
-
-from cmk.agent_based.legacy.v0_unstable import check_levels, LegacyCheckDefinition
+from cmk.agent_based.legacy.conversion import (
+    # Temporary compatibility layer untile we migrate the corresponding ruleset.
+    check_levels_legacy_compatible as check_levels,
+)
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Service,
+)
 from cmk.plugins.couchbase.lib import parse_couchbase_lines, Section
-
-check_info = {}
-
-DiscoveryResult = Iterable[tuple[str, dict]]
 
 
 def discover_couchbase_buckets_items(section: Section) -> DiscoveryResult:
-    yield from ((item, {}) for item, data in section.items() if "curr_items_tot" in data)
+    yield from (Service(item=item) for item, data in section.items() if "curr_items_tot" in data)
 
 
-def check_couchbase_buckets_items(item, params, parsed):
-    if not (data := parsed.get(item)):
+def check_couchbase_buckets_items(
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    if not (data := section.get(item)):
         return
     total_items = data.get("curr_items_tot")
     if total_items is not None:
-        yield check_levels(
+        yield from check_levels(
             int(total_items),
             "items_count",
             params.get("curr_items_tot"),
@@ -36,7 +42,7 @@ def check_couchbase_buckets_items(item, params, parsed):
 
     write_queue = data.get("disk_write_queue")
     if write_queue is not None:
-        yield check_levels(
+        yield from check_levels(
             int(write_queue),
             "disk_write_ql",
             params.get("disk_write_ql"),
@@ -46,7 +52,7 @@ def check_couchbase_buckets_items(item, params, parsed):
 
     fetched = data.get("ep_bg_fetched")
     if fetched is not None:
-        yield check_levels(
+        yield from check_levels(
             int(fetched),
             "fetched_items",
             params.get("fetched_items"),
@@ -56,7 +62,7 @@ def check_couchbase_buckets_items(item, params, parsed):
 
     queue_fill = data.get("ep_diskqueue_fill")
     if queue_fill is not None:
-        yield check_levels(
+        yield from check_levels(
             queue_fill,
             "disk_fill_rate",
             params.get("disk_fill_rate"),
@@ -66,7 +72,7 @@ def check_couchbase_buckets_items(item, params, parsed):
 
     queue_drain = data.get("ep_diskqueue_drain")
     if queue_drain is not None:
-        yield check_levels(
+        yield from check_levels(
             queue_drain,
             "disk_drain_rate",
             params.get("disk_drain_rate"),
@@ -75,11 +81,17 @@ def check_couchbase_buckets_items(item, params, parsed):
         )
 
 
-check_info["couchbase_buckets_items"] = LegacyCheckDefinition(
+agent_section_couchbase_buckets_items = AgentSection(
     name="couchbase_buckets_items",
     parse_function=parse_couchbase_lines,
+)
+
+
+check_plugin_couchbase_buckets_items = CheckPlugin(
+    name="couchbase_buckets_items",
     service_name="Couchbase Bucket %s Items",
     discovery_function=discover_couchbase_buckets_items,
     check_function=check_couchbase_buckets_items,
     check_ruleset_name="couchbase_items",
+    check_default_parameters={},
 )
