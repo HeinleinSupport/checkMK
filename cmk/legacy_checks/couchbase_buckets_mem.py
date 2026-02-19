c3402074a590a33c81130de4d3375ac9381f232b
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-02-19 16:09:24 +0100

    Legacy check migration: couchbase_buckets_mem I
    Change-Id: Id04326e6b0b84ad24e165da90831fff89a02b5d0

--- cmk/legacy_checks/couchbase_buckets_mem.py
+++ cmk/legacy_checks/couchbase_buckets_mem.py
@@ -4,38 +4,42 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 
-from collections.abc import Iterable, Mapping
+from collections.abc import Mapping
 from typing import Any
 
-from cmk.agent_based.legacy.v0_unstable import (
-    check_levels,
-    LegacyCheckDefinition,
-    LegacyCheckResult,
+from cmk.agent_based.legacy.conversion import (
+    # Temporary compatibility layer untile we migrate the corresponding ruleset.
+    check_levels_legacy_compatible as check_levels,
 )
-from cmk.agent_based.v2 import render
-from cmk.legacy_includes.mem import check_memory_element
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    render,
+    Service,
+)
 from cmk.plugins.couchbase.lib import parse_couchbase_lines, Section
-
-check_info: dict[str, Any] = {}
-
-DiscoveryResult = Iterable[tuple[str, dict[str, Any]]]
+from cmk.plugins.lib.memory import check_element
 
 
 def discover_couchbase_buckets_mem(section: Section) -> DiscoveryResult:
     yield from (
-        (item, {}) for item, data in section.items() if "mem_total" in data and "mem_free" in data
+        Service(item=item)
+        for item, data in section.items()
+        if "mem_total" in data and "mem_free" in data
     )
 
 
 def check_couchbase_bucket_mem(
-    item: str, params: Mapping[str, Any], parsed: Section
-) -> LegacyCheckResult:
-    if not (data := parsed.get(item)):
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    if not (data := section.get(item)):
         return
     levels = params.get("levels")
     mode = "abs_used" if isinstance(levels, tuple) and isinstance(levels[0], int) else "perc_used"
     try:
-        yield check_memory_element(
+        yield from check_element(
             "Usage",
             data["mem_total"] - data["mem_free"],
             data["mem_total"],
@@ -47,7 +51,7 @@ def check_couchbase_bucket_mem(
 
     low_watermark = data.get("ep_mem_low_wat")
     if low_watermark is not None:
-        yield check_levels(
+        yield from check_levels(
             low_watermark,
             "mem_low_wat",
             None,
@@ -57,7 +61,7 @@ def check_couchbase_bucket_mem(
 
     high_watermark = data.get("ep_mem_high_wat")
     if high_watermark is not None:
-        yield check_levels(
+        yield from check_levels(
             high_watermark,
             "mem_high_wat",
             None,
@@ -66,9 +70,14 @@ def check_couchbase_bucket_mem(
         )
 
 
-check_info["couchbase_buckets_mem"] = LegacyCheckDefinition(
+agent_section_couchbase_buckets_mem = AgentSection(
     name="couchbase_buckets_mem",
     parse_function=parse_couchbase_lines,
+)
+
+
+check_plugin_couchbase_buckets_mem = CheckPlugin(
+    name="couchbase_buckets_mem",
     service_name="Couchbase Bucket %s Memory",
     discovery_function=discover_couchbase_buckets_mem,
     check_function=check_couchbase_bucket_mem,
