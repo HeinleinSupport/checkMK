421affd78af7a8b3a7f0133621dabb3e08cc0fb3
Rebekka Seltmann <rebekka.seltmann@checkmk.com>
2026-06-25 19:19:47 +0200

    OTel: split REST API endpoints into families and standardize link-relations
    Group the OpenTelemetry REST API endpoints into dedicated endpoint
    families - Receivers, Prometheus scrape, Collector and Quick setup to
    avoid collision in the link relation schemas since the endpoint registry
    uses the family and the link_relation as keys.
    
    Otherwise the receiver and prometheus endpoints are forced to
    artificially use different link relations although they are represent
    the same thing.
    
    CMK-35941
    
    Change-Id: Ia57ca8bddc76301aac2001fa9eb113f2fab719e9

--- cmk/gui/openapi/restful_objects/type_defs.py
+++ cmk/gui/openapi/restful_objects/type_defs.py
@@ -210,7 +210,6 @@ CmkEndpointName = Literal[
     "cmk/save_quick_setup",
     "cmk/edit_quick_setup",
     "cmk/delete_quick_setup",
-    "cmk/delete_prom_scrape",
     "cmk/list_filter_groups",
 ]
 
