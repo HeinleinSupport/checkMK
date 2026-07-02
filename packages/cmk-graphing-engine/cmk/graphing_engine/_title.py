9121ea8e45c95b99c175f6a710cdfc8e33675495
Simon Jess <simon.jess@checkmk.com>
2026-07-02 13:57:49 +0200

    cmk-graphing-engine: rename translated_metrics to performance_data in _title
    Change-Id: I66eaaf7c48cc14f33ee3f2d1da1c64757b1b6427

--- packages/cmk-graphing-engine/cmk/graphing_engine/_title.py
+++ packages/cmk-graphing-engine/cmk/graphing_engine/_title.py
@@ -28,22 +28,22 @@ def _parse_title_expression(raw: str) -> Mapping[str, str]:
 
 def _evaluate_title_expression(
     raw: str,
-    translated_metrics: Mapping[MetricName, PerformanceData],
+    performance_data: Mapping[MetricName, PerformanceData],
 ) -> float | None:
     expression = _parse_title_expression(raw)
-    if (translated := translated_metrics.get(MetricName(expression["metric"]))) is None:
+    if (data := performance_data.get(MetricName(expression["metric"]))) is None:
         return None
     if (scalar := _TITLE_SCALARS.get(expression["scalar"])) is None:
         return None
-    return scalar(translated)
+    return scalar(data)
 
 
 def evaluate_title(
     title: str,
-    translated_metrics: Mapping[MetricName, PerformanceData],
+    performance_data: Mapping[MetricName, PerformanceData],
 ) -> str:
     for raw in _TITLE_EXPRESSION_PATTERN.findall(title):
-        value = _evaluate_title_expression(raw, translated_metrics)
+        value = _evaluate_title_expression(raw, performance_data)
         if value is None:
             return title.split("-", maxsplit=1)[0].strip()
         title = title.replace(raw, str(int(value)), 1)
